0dc66e954ae8e20e7d28e7f118b8d123e037dc16
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2019-01-02 17:49:57 -0600

    Finalized Beast Websockets

--- discordpp/websocket-beast.hh
+++ discordpp/websocket-beast.hh
@@ -11,7 +11,8 @@
 namespace discordpp {
     template<class BASE>
     class WebsocketBeast : public BASE, virtual BotStruct {
-        std::unique_ptr<boost::beast::websocket::stream<ssl::stream<tcp::socket>>> ws_;
+        std::unique_ptr<boost::beast::websocket::stream<ssl::stream < tcp::socket>>>
+        ws_;
         boost::beast::multi_buffer buffer_;
 
         // Report a failure
@@ -21,15 +22,43 @@ namespace discordpp {
 
     public:
         void send(int opcode, json payload) override {
-            ws_->write(boost::asio::buffer(json({{"op", opcode}, {"d",  payload}}).dump()));
+            json tosend = json({{"op", opcode}, {"d", payload}});
+            std::cerr << tosend.dump();
+            ws_->write(boost::asio::buffer(tosend.dump()));
         };
 
     protected:
+        void on_read(boost::system::error_code ec, std::size_t /*bytes_transferred*/) {
+            if (ec) {
+                return fail(ec, "read");
+            }
+
+            json jres;
+            {
+                std::ostringstream ss;
+                ss << boost::beast::buffers(buffer_.data());
+                buffer_.consume(buffer_.size());
+                //std::cerr << "Got " << ss.str() << '\n';
+                jres = json::parse(ss.str());
+            }
+
+            recievePayload(jres);
+
+            ws_->async_read(
+                    buffer_,
+                    [this](boost::system::error_code ec, std::size_t bytes_transferred) {
+                        on_read(ec, bytes_transferred);
+                    }
+            );
+        }
+
         void runctd() override {
             json gateway = call("GET", "/gateway/bot");
-            const std::string url = gateway["url"].get<std::string>() + "/"
+            const std::string url = gateway["url"].get<std::string>().substr(6);
+            /*
+             *  + "/"
                                     + "?v=" + std::to_string(apiVersion)
-                                    + "&encoding=json";
+                                    + "&encoding=json"*/
 
             // The SSL context is required, and holds certificates
             ssl::context ctx{ssl::context::tlsv12};
@@ -39,6 +68,7 @@ namespace discordpp {
             ws_ = std::make_unique<boost::beast::websocket::stream<ssl::stream<tcp::socket>>>(*aioc, ctx);
 
             // Look up the domain name
+            std::cerr << gateway.dump(4) << url << std::endl;
             auto const results = resolver.resolve(url, "443");
 
             // Make the connection on the IP address we get from a lookup
@@ -48,21 +78,15 @@ namespace discordpp {
             ws_->next_layer().handshake(ssl::stream_base::client);
 
             // Perform the websocket handshake
-            ws_->handshake(url, "/");
+            ws_->handshake(url, "/"
+                                "?v=" + std::to_string(apiVersion) +
+                                "&encoding=json"
+            );
 
             ws_->async_read(
                     buffer_,
-                    [this](boost::system::error_code ec, std::size_t /*bytes_transferred*/) {
-                        if (ec) {
-                            return fail(ec, "read");
-                        }
-
-                        json jres;
-                        {
-                            std::ostringstream ss;
-                            ss << boost::beast::buffers(buffer_.data());
-                            jres.parse(ss.str());
-                        }
+                    [this](boost::system::error_code ec, std::size_t bytes_transferred) {
+                        on_read(ec, bytes_transferred);
                     }
             );
 
